699936f74fb4806c73e5a2090df31293b40f85f5
Engrcid <igbegwudavid@gnail.com>
2023-02-22 16:54:56 -0800

    Print Putchar

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print_last_digit-prints the last digit of a number
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ins[] = "_putchar";
+	int i = 0;
+	
+	while (ins[i] != '\0')
+	{
+		_putchar(ins[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
